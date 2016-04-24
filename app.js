d3cde31afa765174a9123e12eb95687b47c35c03
Nat Burgwyn <natburgwyn@Nats-MacBook-Pro.local>
2016-04-23 17:04:51 -0700

    Send the response

--- app.js
+++ app.js
@@ -308,6 +308,21 @@ function sendParksMessage(sender) {
       }
     }
   }
+  request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
 
 function sendButtonMessage(sender, text, buttons) {
