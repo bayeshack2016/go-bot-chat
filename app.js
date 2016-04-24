8a685ffb287d2ba39a6ca0b9639e6e273be40e73
Nat Burgwyn <natburgwyn@Nats-MacBook-Pro.local>
2016-04-23 17:02:19 -0700

    Send Park stub

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ app.post('/webhook/', function (req, res) {
                         case 3:
                           col.updateOne({id:sender},{ $set: { step : 3 } }, function(err, r) {
                                 if (event.postback && event.postback.payload) {
-                                    sendTextMessage(sender, "Sending you parks");
+                                    sendParksMessage(sender);
                                 }
                             });
                             break;
@@ -254,7 +254,60 @@ function sendIntialMessage(sender) {
 }
 
 function sendParksMessage(sender) {
-  
+  messageData = {
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+          {
+            "title":"Classic White T-Shirt",
+            "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+            "subtitle":"Soft white cotton t-shirt is back in style",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                "title":"View Item"
+              },
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+                "title":"Buy Item"
+              },
+              {
+                "type":"postback",
+                "title":"Bookmark Item",
+                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
+              }              
+            ]
+          },
+          {
+            "title":"Classic Grey T-Shirt",
+            "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
+            "subtitle":"Soft gray cotton t-shirt is back in style",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
+                "title":"View Item"
+              },
+              {
+                "type":"web_url",
+                "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
+                "title":"Buy Item"
+              },
+              {
+                "type":"postback",
+                "title":"Bookmark Item",
+                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"
+              }              
+            ]
+          }
+        ]
+      }
+    }
+  }
 }
 
 function sendButtonMessage(sender, text, buttons) {
