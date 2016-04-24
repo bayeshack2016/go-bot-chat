210f0a229213643b7efca950476fb25c6b6ce4c1
Nat Burgwyn <natburgwyn@Nats-MacBook-Pro.local>
2016-04-23 18:53:28 -0700

    Log name

--- app.js
+++ app.js
@@ -278,8 +278,8 @@ function sendParksMessage(sender, doc) {
         var parks = [];
         for (var i = 0; i < 3; i++) {
           if (obj.recareas[i]) {
-            var park = JSON.parse(obj.recareas[i]);
-            console.log(park);
+            //var park = JSON.parse(obj.recareas[i]);
+            console.log(obj.recareas[i].name);
             parks.push({title: obj.recareas[i].name, image_url: obj.recareas[i].image, subtitle: obj.recareas[i].distance, buttons: [{type: "postback", title: "Bookmark Park", payload: obj.recareas[i].id}] });
           }
         }
